ca7b29d2f9721d55144d2f0e45b61c84de926891
cezar <titicezar@gmail.com>
2022-08-25 01:12:00 +0300

    Links are relative instead of absolute

--- server-init.js
+++ server-init.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 
-const port = 8081;
+const port = 80;
 const host = "127.0.0.1";
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
+    console.log("Requesting the site.")
     res.sendFile( __dirname + "/HTML/index.html" );
 });
 app.get('/courses/:course/', function (req, res) {
+    console.log("Requesting the course " + req.params.course + ".")
     res.sendFile( __dirname + "/HTML/courses/" + req.params.course + "/course.html" );
 });
 app.get('/courses/:course/:lesson/', function (req, res) {
+    console.log("Requesting the lesson " + req.params.lesson + ".s")
     res.sendFile( __dirname + "/HTML/courses/" + req.params.course + "/" + req.params.lesson + ".html" );
 });
 
