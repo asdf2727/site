4fccc90647875cd3ce4fdcbfd684e616718e476a
cezar <titicezar@gmail.com>
2022-09-14 13:04:17 +0300

    Worked on the comment submit area

--- server-init.js
+++ server-init.js
@@ -7,15 +7,15 @@ const host = "127.0.0.1";
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    console.log("Requesting the site.")
-    res.sendFile( __dirname + "/HTML/index.html" );
+    console.log("Requesting the main page.")
+    res.sendFile( __dirname + "/HTML/main.html" );
 });
 app.get('/courses/:course/', function (req, res) {
-    console.log("Requesting the course " + req.params.course + ".")
+    console.log("Requesting the course \'" + req.params.course + "\'.")
     res.sendFile( __dirname + "/HTML/courses/" + req.params.course + "/course.html" );
 });
 app.get('/courses/:course/:lesson/', function (req, res) {
-    console.log("Requesting the lesson " + req.params.course + " - " + req.params.lesson + ".")
+    console.log("Requesting the lesson \'" + req.params.course + " - " + req.params.lesson + "\'.")
     res.sendFile( __dirname + "/HTML/courses/" + req.params.course + "/lessons/" + req.params.lesson + ".html" );
 });
 
